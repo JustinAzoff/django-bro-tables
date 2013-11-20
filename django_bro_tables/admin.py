826593f10abb822afe6ae8c104c459e941afa656
Justin Azoff <justin.azoff@gmail.com>
2013-11-20 16:34:15 -0500

    initial admin stuff

--- django_bro_tables/admin.py
+++ django_bro_tables/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django_bro_tables.models import Regex, RegexEntry
-
-class RegexEntryInline(admin.TabularInline):
-    model = RegexEntry
-    extra = 3
+from django_bro_tables.models import (
+    Regex, RegexEntry,
+    Table, TableEntry,
+)
 
 class RegexAdmin(admin.ModelAdmin):
     list_display = ['name', 'comment', 'disabled']
@@ -14,7 +13,20 @@ class RegexEntryAdmin(admin.ModelAdmin):
     list_editable = ['pattern', 'flags', 'comment', 'disabled']
     list_filter = ['regex', 'disabled']
     date_hierarchy = 'date_added'
+
+class TableAdmin(admin.ModelAdmin):
+    list_display = ['name', 'comment', 'num_fields']
+
+COLS = 'c0 c1 c2 c3 c4 c5'.split()
+
+class TableEntryAdmin(admin.ModelAdmin):
+    search_fields = ['pattern', 'comment']
+    list_display = ['table'] + COLS
+    list_editable = COLS
     
 
 admin.site.register(Regex, RegexAdmin)
 admin.site.register(RegexEntry, RegexEntryAdmin)
+
+admin.site.register(Table, TableAdmin)
+admin.site.register(TableEntry, TableEntryAdmin)
