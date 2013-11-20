5848aa460ed809d70808757b63a6de60bdd5edba
Justin Azoff <justin.azoff@gmail.com>
2013-11-20 11:33:51 -0500

    add tables for regex stuff

---
+++ django_bro_tables/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Regex(models.Model):
+    name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=100)
+    disabled = model.BooleanField(default=False)
+
+class RegexEntry(models.Model):
+    regex = models.ForeignKey(Regex)
+    pattern = models.CharField(max_length=200)
+    flags = models.CharField(max_length=10)
+    comment = models.CharField(max_length=100)
+    date_added = models.DateTimeField('date added')
+    disabled = model.BooleanField(default=False)
