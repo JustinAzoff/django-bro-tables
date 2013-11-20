95394d4db58dee006b8188049afb270896d7da7c
Justin Azoff <justin.azoff@gmail.com>
2013-11-20 16:15:19 -0500

    new tables for .. tables :)

--- django_bro_tables/models.py
+++ django_bro_tables/models.py
@@ -18,3 +18,19 @@ class RegexEntry(models.Model):
 
     def __unicode__(self):
         return self.pattern
+
+class Table(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    comment = models.CharField(max_length=100)
+    num_fields = models.SmallIntegerField()
+    #comma seprated list of field names
+    fields = models.CharField(max_length=200)
+
+class TableEntry(models.Model):
+    table = models.ForeignKey(Table, related_name='entries')
+    c0 = models.CharField(max_length=200)
+    c1 = models.CharField(max_length=200, blank=True)
+    c2 = models.CharField(max_length=200, blank=True)
+    c3 = models.CharField(max_length=200, blank=True)
+    c4 = models.CharField(max_length=200, blank=True)
+    c5 = models.CharField(max_length=200, blank=True)
