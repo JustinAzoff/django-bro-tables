98ad13ff986d86aa74c856837ecfc08b4c48da86
Justin Azoff <justin.azoff@gmail.com>
2013-11-22 13:29:49 -0500

    rename the table views to bro_table to fix conflicts with watchdog table

--- django_bro_tables/urls.py
+++ django_bro_tables/urls.py
@@ -7,11 +7,11 @@ router = routers.DefaultRouter()
 router.register(r'regex', views.RegexViewSet)
 router.register(r'regexentry', views.RegexEntryViewSet)
 
-router.register(r'table', views.TableViewSet)
+router.register(r'table', views.TableViewSet,'bro_table')
 
 urlpatterns = patterns('',
     url(r'api/regex/(?P<pk>[0-9]+)/$', views.RegexDetailView.as_view(), name='regex-detail'),
     url(r'api/regex/csv/(?P<name>\S+).csv$', views.RegexFlat.as_view(), name='regex-csv'),
-    url(r'api/table/flat/(?P<name>\S+).csv$', views.TableFlat.as_view(), name='table-flat'),
+    url(r'api/table/flat/(?P<name>\S+).csv$', views.TableFlat.as_view(), name='bro_table-flat'),
     url(r'api/', include(router.urls)),
 )
